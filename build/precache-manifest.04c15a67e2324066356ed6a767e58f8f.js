c95bd10cff77632bace8ac3151650d8d625236f9
samaratungajs <it19244044@my.sliit.lk>
2022-05-16 16:41:48 +0530

    docker file added

---
+++ build/precache-manifest.04c15a67e2324066356ed6a767e58f8f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6bc2b7b0bc0929e75005febe6aebd11",
+    "url": "/checkout/index.html"
+  },
+  {
+    "revision": "8fc516258cb18d169907",
+    "url": "/checkout/static/css/main.bda59e25.chunk.css"
+  },
+  {
+    "revision": "0ce43da136678b6b13ae",
+    "url": "/checkout/static/js/2.681f5af5.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/checkout/static/js/2.681f5af5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8fc516258cb18d169907",
+    "url": "/checkout/static/js/main.ad094f09.chunk.js"
+  },
+  {
+    "revision": "a16e036198f10d413992",
+    "url": "/checkout/static/js/runtime-main.4ed4e385.js"
+  }
+]);
